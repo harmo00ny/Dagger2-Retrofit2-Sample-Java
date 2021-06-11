e0a64f143cafcb22ba81ea7ddc706a43e73d43c0
Harmo00ny <iseekana@gmail.com>
2021-06-11 09:49:38 +0900

    setting environment

---
+++ app/src/main/java/com/marysugar/dagger2_retrofit2_sample_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.marysugar.dagger2_retrofit2_sample_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
